198026906b5a0b99b059762b68d1ea7a600c2db6
Trina <trina@Trinas-MacBook-Pro.local>
2017-05-13 15:53:55 -0700

    About page styling

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,16 @@ var React = require('react');
 //stateless component
 var About = (props) => {
   return(
-    <h3>About New Component</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>This is a Weather Application built on React.</p>
+      <p>Here are the some of the tools I have used</p>
+      <ul>
+        <li><a href="https://facebook.github.io.react">React JS</a> - Javascript Framework</li>
+        <li><a href="http://openweathermap.org">Open Weather Map</a> - Search Weather by city name</li>
+        <li><a href="http://foundation.zurb.com/sites/docs/">Foundation</a> - Styling Framework</li>
+      </ul>
+    </div>
   );
 }
 
