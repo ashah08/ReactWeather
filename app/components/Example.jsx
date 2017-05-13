f509f6b4a6e513077f3295c1867fa8e03e7872f9
Trina <trina@Trinas-MacBook-Pro.local>
2017-05-12 19:15:23 -0700

    Rename Example

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -4,7 +4,7 @@ var React = require('react');
 var Example = (props) =>{
   return(
     <div>
-      <h3>Example Component!</h3>
+      <h3>Examples</h3>
       <p>Please see the examples below</p>
     </div>
   );
